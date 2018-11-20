8bc32966d71dacdb7d8261f71ca14b5112e1d636
Greg Devany <greg.devany@yahoo.com>
2018-11-20 12:11:31 -0600

    Main: Construct Header and SearchContainer

--- src/views/Main.js
+++ src/views/Main.js
@@ -7,11 +7,62 @@ export default class Home extends Component {
     this.state = {};
   }
 
+  //Display the search container
+  searchContainer = () => {
+    return (
+      <div className="searchContainer">
+        <div>What are you searching for?</div>
+
+        <div>
+          <div class="form-check form-check-inline radioButtons">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="peopleOrMovies"
+              id="people"
+              value="option1"
+              checked
+            />
+            <label class="form-check-label" for="exampleRadios1">
+              People
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="peopleOrMovies"
+              id="movies"
+              value="option2"
+            />
+            <label class="form-check-label" for="exampleRadios2">
+              Movies
+            </label>
+          </div>
+        </div>
+        <div />
+        <div>
+          <input
+            className="searchTextBox"
+            placeholder="e.g. Chewbacca, Yoda, Boba Fett"
+          />
+        </div>
+        <div>
+          <button className="searchButton searchButton-active">SEARCH</button>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
-        <div className="header">sdasdf</div>
-        <div className="SearchContainer">Home</div>
+        <div className="header text-center">SWStarter</div>
+        <div className="container">
+          <div className="row justify-content-center">
+            {this.searchContainer()}
+          </div>
+        </div>
       </div>
     );
   }
