878efe9ce0977f2149a5dd423b1cceffe8d3ac4c
Greg Devany <greg.devany@yahoo.com>
2018-11-20 10:57:22 -0600

    App: install Bootstrap

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import "./main.css";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <div className="header">sdasdf</div>
+        <div className="SearchContainer">Home</div>
+      </div>
+    );
   }
 }
